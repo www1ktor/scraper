276116918d29b6d19b91534bbaf816ec3f8c1b34
www1ktor <w.karykowski@icloud.com>
2025-07-09 23:05:55 +0200

    workin on

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
 import csv
+
+chromedriver_autoinstaller.install()
+
+options = Options()
+options.add_argument('--headless')  
+options.add_argument('--disable-gpu')  
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--window-size=1920,1080')
+
+service = Service()
+driver = webdriver.Chrome(options=options)
 
 listings = []
 
@@ -44,82 +59,98 @@ if pages_stop % pages_start != 0:
     n_pages += 1
 
 results = []
+err = []
 
 for page in range(1, n_pages + 1):
-    print(page)
+    #print(page)
     
     response = requests.get(base_url + f"&page={str(page)}", headers=headers)
     soup = BeautifulSoup(response.content, 'lxml')
 
     for thumbnail in soup.find_all(attrs={'data-cy':'listing-item'}):
-        rooms = thumbnail.find(attrs={'data-sentry-component':'RoomsDefinition'})
-        floor = thumbnail.find(attrs={'data-sentry-component':'FloorsDefinition'})
-        title = thumbnail.find(attrs={'data-cy' : 'listing-item-title'})
-        localisation = thumbnail.find(attrs={'data-sentry-component':'Address'})
+        try:
+            rooms = thumbnail.find(attrs={'data-sentry-component':'RoomsDefinition'})
+            floor = thumbnail.find(attrs={'data-sentry-component':'FloorsDefinition'})
+            title = thumbnail.find(attrs={'data-cy' : 'listing-item-title'})
+            localisation = thumbnail.find(attrs={'data-sentry-component':'Address'})
 
-        for dt in thumbnail.find_all("dt"):
-            if "Powierzchnia" in dt.get_text(strip=True):
-                dd = dt.find_next_sibling("dd")
-                if dd:
-                    area = dd
+            for dt in thumbnail.find_all("dt"):
+                if "Powierzchnia" in dt.get_text(strip=True):
+                    dd = dt.find_next_sibling("dd")
+                    if dd:
+                        area = dd
 
-        link = OTODOM_PL[:-1] + thumbnail.find(attrs={'data-cy' : 'listing-item-link'}).get("href")
-        
-        listing = requests.get(link, headers=headers)
-        offer = BeautifulSoup(listing.content, 'lxml')
-
-        id = offer.find(attrs={'data-sentry-element':'DetailsContainer'})
-        price = offer.find(attrs={'data-sentry-element':'Price'})
-        price_per_meter = offer.find(attrs={'aria-label':'Cena za metr kwadratowy'})
-        
-        items = offer.find_all(attrs={'data-sentry-element':"Item"})
-
-        for p in items:
-            label = p.get_text(strip=True).replace(":", "")
-            sibling = p.find_next_sibling()
-            value = sibling.get_text(strip=True) if sibling else None
-
-            if label == 'Czynsz':
-                rent = value
-            elif label == 'Winda':
-                elevator = value
-
-        driver = webdriver.Chrome()
-        driver.get(link)
-        description = str(driver.find_element(By.CSS_SELECTOR, '[data-sentry-element="DescriptionWrapper"]').text)
-        driver.quit()
-        
-        details = {
-            'ID: ' : str(id.get_text(strip=True))[3:].lstrip(' '),
-            'Cena: ' : price, 
-            'Powierzchnia: ' : area,
-            'Cena za metr: ' : price_per_meter, 
-            'Pokoje: ' : rooms,
-            'Lokalizacja: ' : localisation,
-            'Piętro: ' : floor,
-            'Winda: ' : elevator,
-            'Czynsz: ' : rent,
-            'Tytuł: ' : title, 
-            'Opis: ' : description,
-            'Link: ': link
-        }
-
-        for k, v in zip(details.keys(), details.values()):
-            if v and type(v) is not str:
-                v = v.get_text(strip=True)
+            link = OTODOM_PL[:-1] + thumbnail.find(attrs={'data-cy' : 'listing-item-link'}).get("href")
             
-            print(k, v)
+            listing = requests.get(link, headers=headers)
+            offer = BeautifulSoup(listing.content, 'lxml')
+
+            id = offer.find(attrs={'data-sentry-element':'DetailsContainer'})
+            price = offer.find(attrs={'data-sentry-element':'Price'})
+            price_per_meter = offer.find(attrs={'aria-label':'Cena za metr kwadratowy'})
+            
+            items = offer.find_all(attrs={'data-sentry-element':"Item"})
+
+            for p in items:
+                label = p.get_text(strip=True).replace(":", "")
+                sibling = p.find_next_sibling()
+                value = sibling.get_text(strip=True) if sibling else None
+
+                if label == 'Czynsz':
+                    rent = value
+
+                    if rent.startswith('.css'):
+                        rent = "brak informacji"
+
+                elif label == 'Winda':
+                    elevator = value
 
 
-        listings.append(details)
 
-        time.sleep(5)
+            driver.get(link)
+            html = driver.page_source
+            #description = str(driver.find_element(By.CSS_SELECTOR, '[data-sentry-element="DescriptionWrapper"]').text)
+        
+            details = {
+                'ID: ' : str(id.get_text(strip=True))[3:].lstrip(' '),
+                'Cena: ' : price, 
+                'Powierzchnia: ' : area,
+                'Cena za metr: ' : price_per_meter, 
+                'Pokoje: ' : rooms,
+                'Lokalizacja: ' : localisation,
+                'Piętro: ' : floor,
+                'Winda: ' : elevator,
+                'Czynsz: ' : rent,
+                'Tytuł: ' : title, 
+                #'Opis: ' : description,
+                'Link: ': link
+            }
+
+            listing = {}
+
+            for k, v in zip(details.keys(), details.values()):
+                if v and type(v) is not str:
+                    v = v.get_text(strip=True)
+                
+                listing[k] = v
+
+            listings.append(listing)
+            print(listing)
+        
+        except:
+            if link:
+                print("error")
+                err.append(link)
+        
+
+        time.sleep(1)
 
 with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=details.keys())
     writer.writeheader()
     writer.writerows(listings)
 
+print("err's", len(err), err)
  
 
 
