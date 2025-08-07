4730b14b74ada1ce39e802d4989fd3d6d53155ec
www1ktor <w.karykowski@icloud.com>
2025-08-07 22:18:44 +0200

    m

--- sw/test.py
+++ sw/test.py
@@ -8,42 +8,77 @@ from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
 import csv
 
-# Automatyczna instalacja chromedrivera
 chromedriver_autoinstaller.install()
 
-# Konfiguracja opcji Chrome
 options = Options()
-options.add_argument("--headless")  # Nie pokazuj okna przeglądarki
+options.add_argument("--headless") 
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
 
-
-# Start przeglądarki
 driver = webdriver.Chrome(options=options)
 
-# Otwórz stronę
-url = "https://www.otodom.pl/pl/inwestycja/rezydencja-san-petrus-ID4wZGn?priceMax=355553"
-driver.get(url)
+url = ['https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/os-lotnictwa-polskiego-12-ID4u9wv', 'https://www.otodom.pl/pl/oferta/osiedle-naturama-ii-ID4uard', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/osiedle-naturama-ii-ID4uard']
+url = list(set(url))
+links = []
 
-# Poczekaj na załadowanie (ważne przy SPA)
-time.sleep(5)  # Można zastąpić WebDriverWait dla większej precyzji
+OTODOM_PL = "https://www.otodom.pl"
+PRICE_FILTER = "?priceMax=355553"
 
-# Pobierz HTML po załadowaniu JS
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-#print(soup.prettify())
-# Przykład: znajdź wszystkie elementy, np. nagłówki
-while True:
-    for h in soup.find_all(attrs={'data-sentry-element':'StyledAnchor'}):
-        print(h)
-    try:
-        next_btn = driver.find_element(By.XPATH, '//li[@aria-label="Go to next Page"]')
-        driver.execute_script("arguments[0].click();", next_btn)
-        time.sleep(3)  # poczekaj na załadowanie
-    except:
-        break
+for link in url:
+    driver.get(link + PRICE_FILTER)
+    time.sleep(5)      
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    flag = False
 
-# Zamknij przeglądarkę
-driver.quit()
+    while True:
+        for h in soup.find_all(attrs={'data-sentry-element':'StyledAnchor'}):
+            links.append(h.get("href"))
+        for h in soup.find_all(attrs={'data-sentry-element':'Link'}):
+            links.append(h.get("href"))
+        try:
+            next_btn = driver.find_element(By.XPATH, '//li[@aria-label="Go to next Page"]')
+            disabled = next_btn.get_attribute("aria-disabled")
+
+            if disabled == 'true': break
+            
+            driver.execute_script("arguments[0].click();", next_btn)
+            time.sleep(3)  
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'lxml')
+        except:
+            flag = True
+            
+        if flag is True:
+            break
+        
+    print(link, len(links), len(set(links)))
+
+for adv_link in links:
+    id, price, area, price_per_meter, rooms, address, district, administrative_area, city, voivodeship, floor, elevator, rent, title, link = None, None, None, None, None, None, None, None, None, None, None, None, None, None, None
+    #print(OTODOM_PL + adv_link)
+    driver.get(OTODOM_PL + adv_link)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    
+    
+    
+    details = {
+        'ID' : str(id.get_text(strip=True))[3:].lstrip(' '),
+        'Cena' : price, 
+        'Powierzchnia' : area,
+        'Cena za metr' : price_per_meter, 
+        'Pokoje' : rooms,
+        'Ulica' : address.get('Ulica', ''),
+        'Dzielnica' : address.get('Dzielnica', ''),
+        'Obszar administracyjny' : address.get('Obszar administracyjny', ''), 
+        'Miasto' : address.get('Miasto', ''),
+        'Województwo' : address.get('Województwo', ''),
+        'Piętro' : floor,
+        'Winda' : elevator,
+        'Czynsz' : rent,
+        'Tytuł' : title, 
+        'Link': link
+    }
