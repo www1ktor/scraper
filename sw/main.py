491e26c77e1b149422b057e15f9665b07902fd27
www1ktor <w.karykowski@icloud.com>
2025-08-15 22:29:33 +0200

    :(

--- sw/main.py
+++ sw/main.py
@@ -168,19 +168,113 @@ def single():
         for thumbnail in soup.find_all(attrs={'data-sentry-component':'CombinedInvestmentCard'}):
             try:
                 link = OTODOM_PL + thumbnail.find(attrs={'data-cy' : 'listing-item-link'}).get("href")
-                multiadvert_links.append(link)
+                links = []
+                if link not in multiadvert_links:
+                    multiadvert_links.append(link)
+                
+                    driver.get(link + PRICE_FILTER)
+                    time.sleep(5)      
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, 'lxml')
+                    flag = False
+                    print(soup.prettify())
+                    
+                    while True:
+                        for h in soup.find_all(attrs={'data-sentry-element':'StyledAnchor'}):
+                            print(h.prettify())
+                            links.append(h.get("href"))
+                        for h in soup.find_all(attrs={'data-sentry-element':'Link'}):
+                            print(h.prettify())
+                            links.append(h.get("href"))
+                        try:
+                            next_btn = driver.find_element(By.XPATH, '//li[@aria-label="Go to next Page"]')
+                            disabled = next_btn.get_attribute("aria-disabled")
+
+                            if disabled == 'true': break
+                            
+                            driver.execute_script("arguments[0].click();", next_btn)
+                            time.sleep(3)  
+                            html = driver.page_source
+                            soup = BeautifulSoup(html, 'lxml')
+                        except:
+                            flag = True
+                        
+                    for adv_link in links:
+                        time.sleep(1)
+                        try:
+                            id, price, area, price_per_meter, rooms, address, district, administrative_area, city, voivodeship, floor, elevator, rent, title, link = None, None, None, None, None, None, None, None, None, None, None, None, None, None, None
+                            driver.get(OTODOM_PL + adv_link)
+                            html = driver.page_source
+                            soup = BeautifulSoup(html, 'lxml')
+                            
+                            id = soup.find(attrs={'data-sentry-element':'DetailsProperty'}).get_text(strip=True)[3:]
+                            price = soup.find(attrs={'aria-label':'Cena'}).get_text(strip=True)
+                            price_per_meter = soup.find(attrs={'aria-label':'Cena za metr kwadratowy'}).get_text(strip=True)
+                            
+                            for item in soup.find_all(attrs={"data-sentry-element": "Item"}):
+                                label = item.get_text(strip=True).lower()
+                                value_tag = item.find_next_sibling("p")
+                                
+                                if "powierzchnia" in label: area = value_tag.get_text(strip=True)
+                                elif "liczba pokoi" in label: rooms = value_tag.get_text(strip=True)
+                                elif "czynsz" in label:
+                                    rent = value_tag.get_text(strip=True)
+
+                                    if rent.startswith('.css'): rent = "brak informacji"
+                                    elif rent.endswith('zł'): rent = rent.strip()
+
+                                elif "winda" in label: elevator = value_tag.get_text(strip=True)
+                                elif "piętro" in label: floor = value_tag.get_text(strip=True).split('/')[0]    
+                            
+                            localisation = soup.find(attrs={'data-sentry-component':'MapLink'}).get_text(strip=True)
+                            keys = ['Ulica', 'Dzielnica', 'Obszar administracyjny', 'Miasto', 'Województwo'][::-1]
+                            localisation = localisation.split(',')[::-1]
+
+                            address = { key: val for key, val in zip(keys, localisation) }      
+                            
+                            title = soup.find(attrs={'data-cy':'adPageAdTitle'}).get_text(strip=True)
+                            
+                            details = {
+                                'ID' : id,
+                                'Cena' : price, 
+                                'Powierzchnia' : area,
+                                'Cena za metr' : price_per_meter, 
+                                'Pokoje' : rooms,
+                                'Ulica' : address.get('Ulica', ''),
+                                'Dzielnica' : address.get('Dzielnica', ''),
+                                'Obszar administracyjny' : address.get('Obszar administracyjny', ''), 
+                                'Miasto' : address.get('Miasto', ''),
+                                'Województwo' : address.get('Województwo', ''),
+                                'Piętro' : floor,
+                                'Winda' : elevator,
+                                'Czynsz' : rent,
+                                'Tytuł' : title, 
+                                'Link': OTODOM_PL + adv_link
+                            }
+                            
+                            #for k, v in zip(details.keys(), details.values()):
+                                #print(k, v)
+                            #print('\n')
+                            
+                            listings.append(details)  
+                            print(len(listings))   
+                        except:
+                            if link:
+                                err.append(link)   
+                            else:
+                                err.append("ERROR")
         
             except:
                 pass
             
-            time.sleep(1)
+            time.sleep(2)
 
     driver.quit()
 
 #print(list(set(multiadvert_links)))
 
 multiadvert_links = ['https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/os-lotnictwa-polskiego-12-ID4u9wv', 'https://www.otodom.pl/pl/oferta/osiedle-naturama-ii-ID4uard', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/rezydencja-san-petrus-ID4wZGn', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/naramowicka-172-ID4wW7M', 'https://www.otodom.pl/pl/oferta/nadolnik-compact-apartments-ID45iR1', 'https://www.otodom.pl/pl/oferta/osiedle-naturama-ii-ID4uard']
-def multi():
+'''def multi():
     service, driver, soup = None, None, None
     
     links = []
@@ -300,13 +394,12 @@ def multi():
             if link:
                 err.append(link)   
             else:
-                err.append("ERROR")
+                err.append("ERROR")'''
 
     #listings = list(set(listings))
 keys = ['ID', 'Cena', 'Powierzchnia', 'Cena za metr', 'Pokoje', 'Ulica', 'Dzielnica', 'Obszar administracyjny',  'Miasto', 'Województwo', 'Piętro', 'Winda', 'Czynsz', 'Tytuł', 'Link']
 
 single()
-multi()
 
 with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=keys)
